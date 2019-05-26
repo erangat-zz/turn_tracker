96934154612a12167568215f4220c2e6498c6d1c
erangat <erangat@gmail.com>
2019-05-26 23:50:22 +0300

    Add color picker for player card

--- src/app/playerSelect/PlayerCard.jsx
+++ src/app/playerSelect/PlayerCard.jsx
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
+import { TwitterPicker } from "react-color";
+
 import "./PlayerCard.scss";
 
 class PlayerCard extends Component {
+  state = {
+    displayPicker: false,
+    color: this.props.color
+  };
+
+  handleClick = () => {
+    this.setState({ displayPicker: !this.state.displayPicker });
+  };
+
+  handleClose = () => {
+    this.setState({ displayPicker: false });
+  };
+
+  handleChange = color => {
+    this.setState({ color: color.hex });
+  };
+
   render() {
     return (
       <div className="PlayerCard">
         <div className="PlayerCard-info-container">
+          {this.state.displayPicker ? (
+            <div className="PlayerCard-popover">
+              <div className="PlayerCard-cover" onClick={this.handleClose} />
+              <TwitterPicker onChange={this.handleChange} triangle="hide" />
+            </div>
+          ) : null}
           <div className="PlayerCard-picture">
             <p> Pic </p>
           </div>
@@ -18,7 +43,8 @@ class PlayerCard extends Component {
         </div>
         <div
           className="PlayerCard-color"
-          style={{ backgroundColor: this.props.color }}
+          style={{ backgroundColor: this.state.color }}
+          onClick={this.handleClick}
         />
       </div>
     );
