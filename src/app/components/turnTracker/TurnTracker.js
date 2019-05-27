0d1dcd5cdcc1617c8a596dc5dc31e5c508209bda
erangat <erangat@gmail.com>
2019-05-27 23:01:31 +0300

    Change TurnTracker component to functional using hooks

--- src/app/components/turnTracker/TurnTracker.js
+++ src/app/components/turnTracker/TurnTracker.js
@@ -1,36 +1,25 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
 import PlayerSelect from "../playerSelect/PlayerSelect";
 import GameTimers from "../gameTimers/GameTimers";
 import Summary from "../summary/Summary";
 
-class TurnTracker extends Component {
-  state = {
-    step: 1
+function TurnTracker() {
+  const [step, setStep] = useState(1);
+
+  const next = () => {
+    setStep(step + 1);
   };
 
-  next = () => {
-    this.setState({ step: this.state.step + 1 });
-  };
-
-  previous = () => {
-    this.setState({ step: this.state.step - 1 });
-  };
-
-  handle;
-  render() {
-    let { step } = this.state;
-
-    switch (step) {
-      case 1:
-        return <PlayerSelect onNext={this.next} />;
-      case 2:
-        return <GameTimers onNext={this.next} />;
-      case 3:
-        return <Summary />;
-      default:
-        return null;
-    }
+  switch (step) {
+    case 1:
+      return <PlayerSelect onNext={next} />;
+    case 2:
+      return <GameTimers onNext={next} />;
+    case 3:
+      return <Summary />;
+    default:
+      return null;
   }
 }
 
