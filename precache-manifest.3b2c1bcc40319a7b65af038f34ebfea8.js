34dec078b9334a2c5021b9e3007d074126d925b9
erangat <erangat@gmail.com>
2019-05-30 02:37:13 +0300

    Updates

---
+++ precache-manifest.3b2c1bcc40319a7b65af038f34ebfea8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c929cc398f94c5272a64bce77b75d9",
+    "url": "/turn-tracker/index.html"
+  },
+  {
+    "revision": "6d954cdacfe3c7683e1f",
+    "url": "/turn-tracker/static/css/main.0c77eac7.chunk.css"
+  },
+  {
+    "revision": "711e02141c09b482902d",
+    "url": "/turn-tracker/static/js/2.80c7edeb.chunk.js"
+  },
+  {
+    "revision": "6d954cdacfe3c7683e1f",
+    "url": "/turn-tracker/static/js/main.e609b95e.chunk.js"
+  },
+  {
+    "revision": "1c8bcf8ee81e428b46e3",
+    "url": "/turn-tracker/static/js/runtime~main.eaebdf10.js"
+  }
+]);
